def81cca4a333f1caadce5b6dd8566af89f7f882
conradh1 <conradh1@gmail.com>
2017-07-11 08:47:46 -1000

    New Search Engine project.

---
+++ SearchEngine/app.js
@@ -0,0 +1,43 @@
+var searchEngineApp = angular.module('SearchEngineApp', ['ui.router']);
+
+searchEngineApp.config(function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+
+    .state('home', {
+                url: '/home?keywords',
+		templateUrl: 'home.html',
+                controller: 'searchCtrl',
+                params: {
+                    keywords: {
+                        value: '',
+                        squash: true
+                    }
+                },
+                reloadOnSearch: false
+    })
+    
+    .state({name: 'results',
+	    url: '/results?keywords',
+          contorller: 'resultsCtrl',
+          templateUrl: 'results.html'
+    })
+   
+    $urlRouterProvider.otherwise('/home');
+})
+
+searchEngineApp.controller('searchCtrl', function($scope, $state, $stateParams) {
+
+	console.log("Called controller!");
+
+    // function to process the form
+    $scope.submit = function() {
+
+        console.log("Called submit!");
+        //$state.go('results', {keywords: $scope.keywords});
+        $state.go('.', {keywords: $scope.keywords});
+        $scope.showKeywords = function() {
+			return "Results: " + $scope.keywords;
+		};
+    };
+});
