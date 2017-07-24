49273ef1fef12755046967dadb86fdd778f081bf
conradh1 <conradh1@gmail.com>
2017-07-23 18:12:28 -1000

    Got asyn going so request for for mutiple urls.

--- SearchEngine/cgParser.js
+++ SearchEngine/cgParser.js
@@ -1,36 +1,64 @@
-var bodyParser = require('body-parser');
-var request = require('request');
-var cheerio = require('cheerio');
+"use strict";
+
+var request = require('request-promise');
+var cheerio = require('cheerio'); 
+
+
+function entry(city, title, price, link) {
+	this.city = city,
+	this.title = title,
+	this.price = price,
+	this.link = link    
+}
 
 exports.extractHTML = function (req, res) {
 	var jsonData = [];
 	var keywords = req.params.keywords;
-	var title, price, link;
+	var suffixURL = '.craigslist.org/search/cta?query=';
+	var prefixURL = 'https://';
+	var cities = ['honolulu', 'phoenix', 'denver'];
 
 	console.log("Get called Keywords Submitted: "+keywords);
 
 	// url used to search yql
 	var url = 'https://honolulu.craigslist.org/search/cta?query='+keywords;
-	console.log(url);
 
-	// request module is used to process the yql url and return the results in JSON format
-	request(url, function(err, resp, body) {
-		// logic used to compare search results with the input from user
-		console.log("body: "+body);
+	async function main() {  
+    let reqs = cities.map(async function(city) {
+		try {  
+			var body = await request.get(prefixURL+city+suffixURL+keywords);
+			//console.log(body);
+		} catch(err) {
+			console.log('Got an error:', err.message)
+		}
+        
+		//console.log("body: "+body);
+		console.log("city:"+city);
 		var $ = cheerio.load(body);
 		$('li.result-row').each(function(i, element){
-			price = $(element).find('span.result-price').first().text();
-			title = $(element).find('a.hdrlnk').text();
-			link  = $(element).find('a.hdrlnk').attr('href');
+			let price = $(element).find('span.result-price').first().text();
+			let title = $(element).find('a.hdrlnk').text();
+			let link  = $(element).find('a.hdrlnk').attr('href');
 
+// 			var entryObj = new entry(city, price, title, link);
+// 			jsonData.push(JSON.parse(entryObj));
 			var tmp =  {
+				city: city,
 				title: title,
 				price: price,
 				link: link
 			};
 			jsonData.push(tmp);
 		});
-		// pass back the results to client side
-		res.json(jsonData);
 	});
+    await Promise.all(reqs);
+	console.log('Results sent');
+//     jsonData.forEach(function(t) {
+//         console.log(t);
+//     });
+	res.json(jsonData);
+}
+
+	console.log("Calling main");
+	main();  
 };
