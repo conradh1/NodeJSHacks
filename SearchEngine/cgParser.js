e029aa8b5252f3d72f403a074205f46b97453135
conradh1 <conradh1@gmail.com>
2017-07-23 23:38:35 -1000

    Managed to get entries from GL as a summary of averages.

--- SearchEngine/cgParser.js
+++ SearchEngine/cgParser.js
@@ -11,36 +11,96 @@ function entry(city, title, price, link) {
 	this.link = link    
 }
 
-exports.extractHTML = function (req, res) {
+
+exports.getSiteAvg = function (req, res) {
 	var jsonData = [];
 	var keywords = req.params.keywords;
-	var suffixURL = '.craigslist.org/search/cta?query=';
-	var prefixURL = 'https://';
+	var baseURL = 'craigslist.org';
+	var pathURL = '/search/cta?auto_make_model='+keywords;
 	var cities = ['honolulu', 'phoenix', 'denver'];
-
+	
 	console.log("Get called Keywords Submitted: "+keywords);
-
-	// url used to search yql
-	var url = 'https://honolulu.craigslist.org/search/cta?query='+keywords;
 
 	async function main() {  
     let reqs = cities.map(async function(city) {
 		try {  
-			var body = await request.get(prefixURL+city+suffixURL+keywords);
+			var url = 'https://'+city+'.'+baseURL+pathURL;
+			console.log("Searching:"+url);
+			var body = await request.get(url);
+			//console.log(body);
+		} catch(err) {
+			console.log('Got an error:', err.message)
+		}
+    
+		var price_entries = [];
+		//console.log("body: "+body);
+		var $ = cheerio.load(body);
+		$('li.result-row').each(function(i, element){
+			let price = $(element).find('span.result-price').first().text();
+			
+			var x = parseInt(price.replace(/\$/, ''));
+			if (!isNaN(x) && x > 1000 ) {
+				console.log("price|"+x+'|');
+				price_entries.push(x);
+			}
+		});
+		
+		var sum = price_entries.reduce(function(a, b) { return a + b; });
+		var avg = (sum / price_entries.length).toFixed(2);
+		var tmp =  {
+				city: city,
+				Average: avg,
+				Max: Math.max(...price_entries),
+				Min: Math.min(...price_entries),
+				Entries: price_entries.length
+		};
+		jsonData.push(tmp);
+	});
+    await Promise.all(reqs);
+	console.log('Results sent');
+    jsonData.forEach(function(t) {
+		console.log(t);
+    });
+	res.json(jsonData);
+}
+
+	console.log("Calling main");
+	main();  
+};
+
+
+exports.getListings = function (req, res) {
+	var jsonData = [];
+	var keywords = req.params.keywords;
+	var baseURL = 'craigslist.org';
+	var pathURL = '/search/cta?auto_make_model='+keywords;
+	var cities = ['honolulu', 'phoenix', 'denver'];
+
+	console.log("Get called Keywords Submitted: "+keywords);
+
+	async function main() {  
+    let reqs = cities.map(async function(city) {
+		try {  
+			var url = 'https://'+city+'.'+baseURL+pathURL;
+			console.log("Searching:"+url);
+			var body = await request.get(url);
 			//console.log(body);
 		} catch(err) {
 			console.log('Got an error:', err.message)
 		}
         
 		//console.log("body: "+body);
-		console.log("city:"+city);
 		var $ = cheerio.load(body);
 		$('li.result-row').each(function(i, element){
 			let price = $(element).find('span.result-price').first().text();
 			let title = $(element).find('a.hdrlnk').text();
 			let link  = $(element).find('a.hdrlnk').attr('href');
 
-// 			var entryObj = new entry(city, price, title, link);
+			if (/http/.test(link) == false) {
+					link = 'https://'+city+'.'+baseURL+link;
+					console.log(link);
+			}
+// 			var entryObj = new entry(city, title, price, link);
 // 			jsonData.push(JSON.parse(entryObj));
 			var tmp =  {
 				city: city,
@@ -62,3 +122,4 @@ exports.extractHTML = function (req, res) {
 	console.log("Calling main");
 	main();  
 };
+
--- SearchEngine/server.js
+++ SearchEngine/server.js
@@ -1,9 +1,7 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var request = require('request');
-var cheerio = require('cheerio');
 var app = express();
 var cgParser = require('./cgParser.js');
+var bodyParser = require('body-parser')
 
 /* ==========================================================
 serve the static index.html from the public folder
@@ -20,5 +18,5 @@ app.listen(8000, function() {
 handle the Search function
 ============================================================ */
 app.get('/home/:keywords', function (req, res) {
-	cgParser.extractHTML(req, res);
+	cgParser.getSiteAvg(req, res);
 });
